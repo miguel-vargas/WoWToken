a93125fb70b58f7d981fdd64a9660b39d36d3924
Miguel <migs.a.vargas@gmail.com>
2019-06-27 23:44:52 -0400

    Session Storage added
    SharedPreferences added to the onCreateView, though will probably need to be moved to onResume just like mainActivity is doing so. Some basic error handling if Access token is empty. ButterKnife used to set the regionPrice text.

--- app/src/main/java/com/migs/wowtokenprice/ui/main/NAFragment.java
+++ app/src/main/java/com/migs/wowtokenprice/ui/main/NAFragment.java
@@ -1,16 +1,20 @@
 package com.migs.wowtokenprice.ui.main;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.migs.wowtokenprice.R;
 import com.migs.wowtokenprice.api.model.WoWTokenAPI;
 import com.migs.wowtokenprice.api.model.WoWTokenResponse;
+import com.migs.wowtokenprice.storage.Session;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +29,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * A simple {@link Fragment} subclass.
  */
 public class NAFragment extends Fragment {
+
+    public static final String WOW_TOKEN_PREFERENCES = "WoWTokenPreferences";
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String NA_REGION_NAMESPACE = "dynamic-us";
+
+    String accessToken = "";
+
+    SharedPreferences sharedPreferences;
 
     @BindView(R.id.region) TextView region;
 
@@ -62,6 +74,12 @@ public class NAFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_region, container, false);
 
+        sharedPreferences = getActivity().getSharedPreferences(WOW_TOKEN_PREFERENCES, Context.MODE_PRIVATE);
+
+        if (sharedPreferences.contains(ACCESS_TOKEN)){
+            accessToken = sharedPreferences.getString(ACCESS_TOKEN,"");
+        }
+
         unbinder = ButterKnife.bind(this, rootView);
 
 //        getToken();
@@ -78,15 +96,26 @@ public class NAFragment extends Fragment {
     }
 
     public void getToken() {
-        WoWTokenAPI.Factory.getInstance().getWoWToken().enqueue(new Callback<WoWTokenResponse>() {
+        if (accessToken.isEmpty()) {
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "Error: WoW Token is not authenticated with Blizzard API. Try again.", Toast.LENGTH_LONG).show();
+            return;
+        }
+        accessToken = "Bearer " + accessToken;
+
+        WoWTokenAPI.Factory.getInstance().getWoWToken(accessToken, NA_REGION_NAMESPACE) .enqueue(new Callback<WoWTokenResponse>() {
             @Override
             public void onResponse(Call<WoWTokenResponse> call, Response<WoWTokenResponse> response) {
-
+                if (response.body() != null) {
+                    int tokenPrice = response.body().getPriceInGold();
+                    regionPrice.setText(tokenPrice);
+                }
             }
 
             @Override
             public void onFailure(Call<WoWTokenResponse> call, Throwable t) {
-
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "Error: No NA Response", Toast.LENGTH_LONG).show();
             }
         });
     }
