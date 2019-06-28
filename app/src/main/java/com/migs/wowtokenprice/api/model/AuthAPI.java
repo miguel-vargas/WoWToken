43f1b995f3717e1ac1a9d8e90888a002891ccee4
Miguel <migs.a.vargas@gmail.com>
2019-06-27 23:33:23 -0400

    Added necessary parameters for AuthRequest
    Removed @Header parameter from the getAuth() function as the header needs to be generated at runtime. Changed the HTTP method to POST. Added @FormUrlEncoded annotation to the method. Added a @Field parameter with key grant_type. Client credentials will be the value needed (maybe overload that function to provide a default value).

--- app/src/main/java/com/migs/wowtokenprice/api/model/AuthAPI.java
+++ app/src/main/java/com/migs/wowtokenprice/api/model/AuthAPI.java
@@ -1,28 +1,36 @@
 package com.migs.wowtokenprice.api.model;
 
+import com.migs.wowtokenprice.BuildConfig;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.GET;
-import retrofit2.http.Header;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 
 public interface AuthAPI {
     String BASE_URL = "https://us.battle.net/oauth/";
-    String authorization = "Authorization: Basic XXXXXX";
-    String contentType = "Content-Type: application/x-www-form-urlencoded";
 
-
-    @GET("token")
-    Call<AuthResponse> getAuth(@Header("Authorization") String authHeader);
+    @FormUrlEncoded
+    @POST("token")
+    Call<AuthResponse> getAuth(@Field("grant_type") String grantType);
+//    Call<AuthResponse> getAuth(@Header("Authorization") String authHeader);
 
     class Factory {
         private static AuthAPI service;
 
         public static AuthAPI getInstance() {
             if (service == null) {
+                OkHttpClient client = new OkHttpClient().newBuilder()
+                        .addInterceptor(new BasicAuthInterceptor(BuildConfig.ClientID, BuildConfig.ClientSecret))
+                        .build();
+
                 Retrofit retrofit = new Retrofit.Builder()
                         .addConverterFactory(GsonConverterFactory.create())
                         .baseUrl(BASE_URL)
+                        .client(client)
                         .build();
 
                 service = retrofit.create(AuthAPI.class);
