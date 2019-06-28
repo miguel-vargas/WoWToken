b901ffdc334100c7624293599837d77cefb4b8c3
Miguel <migs.a.vargas@gmail.com>
2019-06-27 23:37:55 -0400

    Fixed typo
    Changed expired_in to expires_in.

--- app/src/main/java/com/migs/wowtokenprice/api/model/AuthResponse.java
+++ app/src/main/java/com/migs/wowtokenprice/api/model/AuthResponse.java
@@ -3,7 +3,7 @@ package com.migs.wowtokenprice.api.model;
 public class AuthResponse {
     private String access_token;
     private String token_type;
-    private int expired_in;
+    private int expires_in;
 
     public void setAccess_token(String access_token) {
         this.access_token = access_token;
@@ -13,7 +13,8 @@ public class AuthResponse {
         this.token_type = token_type;
     }
 
-    public void setExpired_in(int expired_in) {
-        this.expired_in = expired_in;
+    public void setExpires_in(int expires_in) {
+        this.expires_in = expires_in;
+    }
     }
 }
